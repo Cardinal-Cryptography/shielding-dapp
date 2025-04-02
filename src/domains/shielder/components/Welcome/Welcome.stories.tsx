8542571f2e71a4e22c0fd9a4a9181304d0f74cf7
rAskVAL <roberts.ribakovs@cardinals.cc>
2025-04-02 21:42:58 +0300

    SD-33: Create welcome screen component

---
+++ src/domains/shielder/components/Welcome/Welcome.stories.tsx
@@ -0,0 +1,13 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import Welcome from 'src/domains/shielder/components/Welcome/Welcome.tsx';
+
+const meta: Meta<typeof Welcome> = {
+  component: Welcome,
+};
+
+export default meta;
+
+export const Default: StoryObj<typeof Welcome> = {
+
+};
