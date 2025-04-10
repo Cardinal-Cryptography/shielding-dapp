f346da2288480ec22a2ebc632829bc6239bff24c
Piotr Dżaluk <piotr.dzaluk@cardinals.cc>
2025-04-10 14:22:39 +0200

    SD-44: Source app version from git tag

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 /// <reference types="vitest" />
+import childProcess from 'child_process';
 import fs from 'fs';
 import { IncomingMessage, ServerResponse } from 'node:http';
 import path from 'path';
 
 import react from '@vitejs/plugin-react';
+import semverRegex from 'semver-regex';
 import { objectEntries, objectFromEntries } from 'tsafe';
 import { defineConfig, loadEnv, normalizePath } from 'vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
@@ -12,7 +14,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import { parse } from 'yaml';
 
 import envVarsSchema from './envVarsSchema';
-import { version } from './package.json';
 const customHttpPath = path.resolve(__dirname, 'customHttp.yml');
 const customHttpContent = fs.readFileSync(customHttpPath, 'utf8');
 const { customHeaders } = parse(customHttpContent) as {
@@ -87,6 +88,51 @@ export default defineConfig({
         };
       },
     },
+    {
+      name: 'app-version',
+      config: async () => {
+        const [
+          tag,
+          commitHash,
+        ] = await Promise.all([
+          new Promise<string | undefined>((resolve, reject) => {
+            childProcess.exec('git tag --points-at HEAD', (err, stdout) => {
+              if (err) reject(err);
+              else {
+                const probablyMostRecentTag =
+                  // eslint-disable-next-line no-restricted-syntax
+                  stdout
+                    .trim()
+                    .split('\n')
+                    .sort() // we can't determine the chronological order of lightweight tags, so we hope that the alphabetical order matches chronology
+                    .at(-1);
+
+                resolve(probablyMostRecentTag);
+              }
+            });
+          }),
+          new Promise<string>((resolve, reject) => {
+            childProcess.exec('git rev-parse --short HEAD', (err, stdout) => {
+              if (err) reject(err);
+              else resolve(stdout.trim());
+            });
+          }),
+        ]);
+
+        if (tag && !semverRegex().test(tag)) {
+          throw new Error(`The tag "${tag}" does not follow the semver format.`);
+        }
+
+        // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
+        const appVersion = tag || commitHash;
+
+        return {
+          define: {
+            'import.meta.env.APP_VERSION': JSON.stringify(appVersion),
+          },
+        };
+      },
+    },
   ],
   optimizeDeps: {
     exclude: ['@cardinal-cryptography/shielder-sdk-crypto-wasm'],
@@ -94,8 +140,5 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     include: ['src/**/*.test.[jt]s?(x)'],
-  },
-  define: {
-    'import.meta.env.APP_VERSION': JSON.stringify(version),
   },
 });
