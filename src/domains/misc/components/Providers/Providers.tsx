7c0b1c4b1f60a98a425d207542e0f4cd9ac7d086
Piotr Dżaluk <piotr.dzaluk@cardinals.cc>
2025-04-11 09:39:57 +0200

    SD-47: Fix unsupported chain connection

--- src/domains/misc/components/Providers/Providers.tsx
+++ src/domains/misc/components/Providers/Providers.tsx
@@ -1,20 +1,23 @@
 import { createAppKit } from '@reown/appkit/react';
 import { LazyMotion, domAnimation } from 'motion/react';
 import { ReactNode } from 'react';
+import { values } from 'remeda';
 import { Toaster } from 'sonner';
 import { WagmiProvider } from 'wagmi';
 
 import WalletProvider from 'src/domains/chains/components/WalletProvider';
 import { wagmiAdapter } from 'src/domains/chains/utils/clients';
-import definitions, { Definition } from 'src/domains/chains/utils/definitions';
+import definitions from 'src/domains/chains/utils/definitions';
 import { QueryClientProvider } from 'src/domains/misc/utils/queryClient';
 import WasmProvider from 'src/domains/shielder/utils/WasmProvider';
 
 import GlobalStylesWithTheme from './GlobalStylesWithTheme';
 
+const allSupportedChains = values(definitions).flatMap(definition => values(definition));
+
 createAppKit({
   adapters: [wagmiAdapter],
-  networks: wagmiAdapter.wagmiChains as [Definition, ...Definition[]],
+  networks: [allSupportedChains[0], ...allSupportedChains],
   defaultNetwork: definitions.alephZero.testnet,
   projectId: import.meta.env.PUBLIC_VAR_REOWN_PROJECT_ID,
   features: {
