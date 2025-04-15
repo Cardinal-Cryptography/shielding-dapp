8a2e6e1eb192bc61234ede07ca700c6ca7158dc8
rAskVAL <roberts.ribakovs@cardinals.cc>
2025-04-15 13:02:14 +0300

    SD-45: Add supported wallets to featured wallets list

--- src/domains/misc/components/Providers/Providers.tsx
+++ src/domains/misc/components/Providers/Providers.tsx
@@ -20,12 +20,15 @@ createAppKit({
   termsConditionsUrl: TERMS_OF_SERVICE_LINK,
   privacyPolicyUrl: PRIVACY_POLICY_LINK,
   enableWalletGuide: false,
-  includeWalletIds: [
+  // Using `featuredWalletIds` instead of `includeWalletIds`, as `includeWalletIds` still displays all installed wallets.
+  featuredWalletIds: [
     'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96', //MetaMask,
     'fd20dc426fb37566d803205b19bbc1d4096b248ac04548e3cfb6b3a38bd033aa', //Coinbase
     'e0c2e199712878ed272e2c170b585baa0ff0eb50b07521ca586ebf7aeeffc598', //Talisman
     '18388be9ac2d02726dbac9777c96efaac06d744b2f6d580fccdd4127a6d01fd1', //Rabby
   ],
+  // Hiding the "All Wallets" button so only featured wallets are shown.
+  allWallets: 'HIDE',
   chainImages: wagmiAdapter.wagmiChains ?
     Object.fromEntries(wagmiAdapter.wagmiChains.map(c => [c.id, `/chains/${c.id}.svg`])) :
     undefined,
