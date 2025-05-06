14f66114a41101ccc05288cd703c256d1a72ebbf
rAskVAL <roberts.ribakovs@cardinals.cc>
2025-05-06 14:49:43 +0300

    SD-70: Sanitize addresses from

--- src/domains/misc/components/Providers/PostHogProvider.tsx
+++ src/domains/misc/components/Providers/PostHogProvider.tsx
@@ -6,11 +6,39 @@ type Props = {
   children: ReactNode,
 };
 
-const PostHogProvider = ({ children }: Props) => {
+const WALLET_REGEX = /0x[a-fA-F0-9]{40}/g;
 
+// eslint-disable-next-line @stylistic/comma-dangle
+const sanitizeAddresses = <T,>(input: T): T => {
+  if (typeof input === 'string') {
+    return input.replace(WALLET_REGEX, '<<sanitized address>>') as T;
+  }
+
+  if (Array.isArray(input)) {
+    return input.map(sanitizeAddresses) as T;
+  }
+
+  if (typeof input === 'object' && input !== null) {
+    return Object.fromEntries(
+      Object.entries(input).map(([key, value]) => [key, sanitizeAddresses(value)])
+    ) as T;
+  }
+
+  return input;
+};
+
+const PostHogProvider = ({ children }: Props) => {
   const posthogOptions: Partial<PostHogConfig> = {
     api_host: import.meta.env.PUBLIC_VAR_POSTHOG_HOST,
     persistence: 'memory',
+    before_send: event => {
+      if (!event) return null;
+
+      return {
+        ...event,
+        properties: sanitizeAddresses(event.properties),
+      };
+    },
   };
 
   return (
